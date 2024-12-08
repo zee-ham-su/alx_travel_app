ede2f242dba16938b48b85c0fe9fa422ff2cde8f
zee-ham-su <hsufiian@yahoo.com>
2024-12-08 00:17:35 +0000

    django project setup

---
+++ alx_travel_app/alx_travel_app/celery.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+
+app = Celery('alx_travel_app')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
